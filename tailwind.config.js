faf2791994175c6db68db68b595fa27a661f8c4f
Bashbory <eightmick912@gmail.com>
2023-06-16 17:22:38 +0300

    card

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.{html,js}',
+'./index.html'],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Fraunces':['Fraunces'],
+        'Montserrat':['Montserrat'],
+
+      },
+    },
+  },
+  plugins: [],
+}
+
